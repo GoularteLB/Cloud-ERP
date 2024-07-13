4c34037b8239589ddd1c631467e6c278ff0adc26
Lucas <goulartelucasbeluco@gmail.com>
2024-07-13 18:45:26 -0300

    CloudERP

---
+++ clouderp/src/main/java/com/cloud/clouderp/ClouderpApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.clouderp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClouderpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClouderpApplication.class, args);
+	}
+
+}
